94f475b36b2c7f42f7b57464322ea83aaaf3c23d
Skillhh <ortegon.giovanny@hotmail.com>
2019-05-05 00:26:26 -0500

    Actualizando

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
     context = {
-        'status':'Working on the project'
+        'contacts':Contact.objects.all()
     }
     return render(request, 'index.html', context)
     
